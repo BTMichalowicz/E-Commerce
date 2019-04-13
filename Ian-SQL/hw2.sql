be4c236a62090fd20d786d7a4bc6d3cfa35287fa
irpeitzsch <31490121+irpeitzsch@users.noreply.github.com>
2019-04-13 01:21:56 -0400

    Update hw2.sql
    Changed some things. Added Address table, this way when a customer is added they don't need an address associated with them yet, But once one is associated with them, then all of the information is associated with them. Also made credit card a big int.

--- Ian-SQL/hw2.sql
+++ Ian-SQL/hw2.sql
@@ -10,53 +10,60 @@ CREATE TABLE Seller(
 CREATE TABLE Item(
 	ItemId INT AUTO_INCREMENT,
     Price DECIMAL(10,2) DEFAULT 0.00,
-    ItemType VARCHAR(45),
+    ItemType VARCHAR(45) default '',
     Quantity INT DEFAULT 0,
-    ItemName VARCHAR(45),
+    ItemName VARCHAR(45) default 'Item',
     SellerId INT NOT NULL,
     Primary Key(ItemId),
     Foreign Key(SellerId) References Seller(SellerId) );
     
+create table Address(
+	AddId int auto_increment,
+	Address varchar(45) not null,
+    Town varchar(45) not null,
+    State char(2) not null,
+    ZIP int not null,
+    primary key(AddID) );
+    
 create table Customer(
 	CustomerId int auto_increment,
-    FirstName varchar(45),
-    LastName varchar(45),
+    FirstName varchar(45) not null,
+    LastName varchar(45) not null,
     Email varchar(45),
-    Address varchar(45),
-    primary key(CustomerId) );
-    
+    Address int,
+    primary key(CustomerId), 
+    foreign key(Address) references Address(AddId));
+ 
 create table Reviews(
 	CustomerId int,
     ItemId int,
-    SellerId int,
-    Rating int,
+    Rating int not null,
     Review varchar(256),
-    primary key(CustomerId, ItemId, SellerId),
+    primary key(CustomerId, ItemId),
     foreign key(CustomerId) references Customer(CustomerId),
-    foreign key(ItemId) references Item(ItemId),
-    foreign key(SellerId) references Seller(SellerId) );
+    foreign key(ItemId) references Item(ItemId) );
     
 create table Shipment(
 	ShipmentId int auto_increment,
-    ShipmentAddress varchar(256),
-    ZipCode int,
-    ShipmentStatus char(9),
+    ShipmentAddress int not null,
+    ShipmentStatus char(9) not null,
     primary key(ShipmentId),
-    check(ShipmentStatus in ('ARRIVED', 'PROCESSED', 'SHIPPED')) );
+    check(ShipmentStatus in ('ARRIVED', 'PROCESSED', 'SHIPPED')),
+    foreign key(ShipmentAddress) references Address(AddID) );
 
 create table Employee(
 	EmployeeId int auto_increment,
     EmployeeRole varchar(64),
-    FirstName varchar(45),
-    LastName varchar(45),
-    Joined date,
+    FirstName varchar(45) not null,
+    LastName varchar(45) not null,
+    Joined date not null,
     SupervisorId int,
     primary key(EmployeeId),
     foreign key(SupervisorId) references Employee(EmployeeId),
     check(SupervisorId != EmployeeId) );
 
 create table Payment(
-    CreditCard int,
+    CreditCard bigint,
     PaymentType varchar(10) not null,
     ExpirationDtae date not null,
     primary key(CreditCard),
@@ -65,9 +72,9 @@ create table Payment(
 create table Buys(
 	CustomerId int,
     ItemId int,
-    Quantity int,
+    Quantity int not null,
     Price decimal(10,2),
-    PaymentId int,
+    PaymentId bigint,
     primary key(CustomerId, ItemId),
     foreign key(CustomerId) references Customer(CustomerId),
     foreign key(ItemId) references Item(ItemId),
