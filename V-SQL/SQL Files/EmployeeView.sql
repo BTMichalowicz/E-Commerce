115248c4c59c7475e36785c6aeb45a2f28e360c8
DESKTOP-VGI7DEH\vquin <veronica.quintana@stonybrook.edu>
2019-04-14 14:12:42 -0400

    V .sql files added, documentation to be created.

--- V-SQL/SQL Files/EmployeeView.sql
+++
@@ -1,1 +0,0 @@
-CREATE VIEW `Admin/Employee` AS
---
+++ V-SQL/insertions.sql
@@ -0,0 +1,22 @@
+-- Model: E-Commerce    Version: 1.0
+-- MySQL Workbench Forward Engineering
+
+USE ECOMMERCE;
+
+INSERT INTO CUSTOMER VALUES (1, 'V', 'Quintz', 623, 123456, 'nah');
+INSERT INTO EMPLOYEE VALUES (1, 1, 'Professional Nerd', STR_TO_DATE('4-13-2019', '%m-%d-%Y'));
+INSERT INTO INVENTORY VALUES (1, 'Dude McGuy', 35.5, 'Key to City', 2);
+INSERT INTO ITEM VALUES (1, 'Pirate Treasure', 15.2, 'Dude McGuy');
+INSERT INTO PAYMENT VALUES (123456789, 'SoMuchInterestCard', 072020);
+INSERT INTO REVIEWS VALUES(1, 2, 3, 'BRUH.', 1, 4.5);
+INSERT INTO SHIPMENTDELIVERY VALUES(5, 'ExpressShip', '555 Yes St.', 'Paid', 34.66);
+INSERT INTO SHOPPINGCART VALUES(1, 6.23, 12.46, 'Will To Live', 2);
+
+
+
+
+
+
+
+
+
