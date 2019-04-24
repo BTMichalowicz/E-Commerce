0e643761ac57d1b3bf63961e339d8bf076c2ac2f
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2019-04-24 09:12:08 -0400

    Working on create table statements inside 'finalProj/app.js'; up next, backend inside routes as well as some transaction processing. Following the tutorial that is

--- finalProj/app.js
+++ finalProj/app.js
@@ -24,15 +24,41 @@ db.connect((err) => {
 	console.log('Welcome to Database Designers Pro!');
 });
 
-db.query('drop database if exists myDB ;', (err) => {if(err) {throw err;} console.log("Using Relational Database");  } );
-db.query('create Database myDB ;', (err) => {if(err) {throw err;} console.log("Using Relational Database");  } );
+//db.query('drop database if exists myDB ;', (err) => {if(err) {throw err;} console.log("Dropping Relational Database");  } );
+db.query('create Database if not exists myDB ;', (err) => {if(err) {throw err;} console.log("Creating Relational Database");  } );
 db.query('use myDB ;', (err) => {if(err) {throw err;} console.log("Using Relational Database");  } );
 
 //db.query('use myDB ;', (err) => {if(err) {throw err;} console.log("Using Relational Database");  } );
 
 
 //Table creation
-db.query('CREATE TABLE Seller(SellerId INT AUTO_INCREMENT,  SellerName VARCHAR(64) DEFAULT "Anonymous", Primary Key(SellerId) );',(err) => {if(err) {throw err;} console.log( "Created Seller Table")});
+db.query('CREATE TABLE if not exists Seller(SellerId INT AUTO_INCREMENT,  SellerName VARCHAR(64) DEFAULT "Anonymous", Primary Key(SellerId) ) AUTO_INCREMENT=1;',(err) => {if(err) {throw err;} console.log( "Created Seller Table")});
 
-db.query('drop database if exists myDB ;', (err) => {if(err) {throw err;} console.log("dropping Relational Database");  } );
-//db.end()
+db.query('CREATE TABLE if not exists Item(	ItemId INT AUTO_INCREMENT,    Price DECIMAL(10,2) DEFAULT 0.00,    ItemType VARCHAR(45) default \'\',    Quantity INT DEFAULT 0,    ItemName VARCHAR(45) default \'Item\',    SellerId INT NOT NULL,    Primary Key(ItemId),    Foreign Key(SellerId) References Seller(SellerId) ) AUTO_INCREMENT=1;',(err) => {if(err) {throw err;} console.log( "Created Item Table")});
+
+db.query('CREATE TABLE if not exists Address(	AddId int auto_increment,	Address varchar(45) not null,    Town varchar(45) not null,    State char(2) not null,    ZIP int not null,    primary key(AddID) ) AUTO_INCREMENT=1;',(err) => {if(err) {throw err;} console.log( "Created Address Table")});
+
+db.query('CREATE TABLE if not exists Customer(	CustomerId int auto_increment,    FirstName varchar(45) not null,    LastName varchar(45) not null,    Email varchar(45),    Address int,    primary key(CustomerId),     foreign key(Address) references Address(AddId)) AUTO_INCREMENT=1;',(err) => {if(err) {throw err;} console.log( "Created Customer Table")});
+
+db.query('CREATE TABLE if not exists Reviews(	CustomerId int,    ItemId int,    Rating int not null,    Review varchar(256),    primary key(CustomerId, ItemId),    foreign key(CustomerId) references Customer(CustomerId),    foreign key(ItemId) references Item(ItemId) );', (err) => {if (err) {throw err;} console.log("Created Reviews Table")});
+
+
+
+
+
+
+
+
+
+
+//TODO: CREATE STATEMENTS
+
+
+global.db=db; //Global DB variable
+
+
+
+
+//For testing purposes
+//db.query('drop database if exists myDB ;', (err) => {if(err) {throw err;} console.log("dropping Relational Database");  } );
+db.end()
