3680bb42fe28e19f648727206c2acc1fa909c147
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2019-05-02 10:36:09 -0400

    Add Seller,  Add Item, Delete of both with attempt at cascade; pull before pushing

--- finalProj/routes/index.js
+++ finalProj/routes/index.js
@@ -107,7 +107,57 @@ module.exports = {
                 });
             
         
-    }
+    },
+
+     deleteItem: (req, res) => {
+        let ItemId = req.params.ItemId;
+        
+        let deleteUserQuery = 'DELETE FROM Item WHERE ItemId = '+ ItemId;
+       
+
+       
+                db.query(deleteUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/list_Items');
+                });
+            
+        
+    }, 
+
+    addItemPage: (req, res) =>{
+        res.render('add_item.ejs', {
+            title: "Add an Item!!!",
+            message: ''
+        });
+    },
+
+    addItem: (req, res) => {
+          
+        if(req.body.ItemName == '' || req.body.Price == '' || req.body.ItemType == '' || req.body.Quantity == '' || req.body.SellerId == ''){
+            res.direct('list_items.ejs');
+        }
+
+        let message1 ='';
+        let ItemName = req.body.ItemName;
+        let Price = req.body.Price;
+        let ItemType = req.body.ItemType;
+        let Quantity = req.body.Quantity;
+        let SellerId = req.body.SellerId;
+
+
+         let query = "INSERT INTO Item (Price, ItemType,Quantity,ItemName, SellerId) VALUES (" + Price + ",'" + ItemType + "'," + Quantity + ",'" + ItemName + "'," + SellerId + ")";
+
+                db.query(query, (err,result) => {
+                    if(err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/list_Items');
+                });
+
+        
+    },
 
 
 };
