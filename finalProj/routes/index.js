528546b2c21f71da37e02fa1c822a29457abba4b
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2019-05-05 14:04:30 -0400

    Login works and registration works and everything

--- finalProj/routes/index.js
+++ finalProj/routes/index.js
@@ -5,284 +5,305 @@ var router = express.Router()
 let user = "NULL";
 module.exports = {
 
-    getBuy: (req, res) => {
-      let q = "select B.CustomerId, B.ItemId, I.ItemName, B.Quantity, B.Price from Buys B, Item I where B.CustomerId = " + user + " and B.ItemId = I.ItemId and B.PaymentId = NULL;";
-      db.query(q, (err, result) => {
+  getBuy: (req, res) => {
+    let q = "select B.CustomerId, B.ItemId, I.ItemName, B.Quantity, B.Price from Buys B, Item I where B.CustomerId = " + user + " and B.ItemId = I.ItemId and B.PaymentId = NULL;";
+    db.query(q, (err, result) => {
+      if(err) {
+        return res.status(500).send(err);
+      }
+      res.render('transaction.ejs', {
+        title: "Shopping Cart",
+        Buys: result
+      });
+
+    });
+  },
+  getHome: (req, res) => {
+
+    res.render('index.ejs', {
+      title: "Database Designers Pro!"
+    });
+
+  },
+
+  getItem: (req, res) => {
+
+   let ItemQ = "SELECT * FROM Item ORDER BY ItemID ASC";
+   db.query(ItemQ, (err, result) => {
+     if (err) res.redirect('/');
+
+     res.render('list_items.ejs', {title: "List Items",
+      Item: result
+    });
+
+   });
+ },
+
+ getSellerPage:  (req, res) => {
+  let ItemQ = "SELECT * FROM Seller ORDER BY SellerId ASC";
+  db.query(ItemQ, (err, result) => {
+    if (err) res.redirect('/');
+
+    res.render('list_sellers.ejs', {title: "List Sellers!",
+      Seller: result
+    });
+
+  });
+},
+
+
+getSeller: (req, res) => {
+ let ItemQ = "SELECT * FROM Seller ORDER BY SellerId ASC";
+ db.query(ItemQ, (err, result) => {
+   if (err) res.redirect('/');
+
+   res.render('list_sellers.ejs', {title: "List Sellers!",
+    Seller: result
+  });
+
+ });
+},
+
+addSellerPage: (req, res) =>{
+  res.render('add_seller.ejs', {
+    title: "Add a Seller!",
+    message: ''
+  });
+},
+
+addSeller: (req, res) => {
+
+  if(req.body.SellerName == ''){
+    res.direct('list_sellers.ejs');
+  }
+
+  let message1 ='';
+  let SellerName = req.body.SellerName;
+
+  let nameQuery= "Select * from Seller where SellerName = '" + SellerName + "'";
+  db.query(nameQuery, (err, result)=>{
+    if(err){
+      return res.status(500).send(err);
+    }
+
+    if(result.length >0 && SellerName != null){
+      message1 = 'Name already exists in the database or is null';
+      res.render('add_seller.ejs',{
+        message: message1,
+        title: "Add a Seller!"
+      });
+
+    } else {
+
+      let query = "INSERT INTO Seller (SellerName) VALUES ('" + SellerName + "')";
+
+      db.query(query, (err,result) => {
         if(err) {
           return res.status(500).send(err);
         }
-        res.render('transaction.ejs', {
-          title: "Shopping Cart",
-          Buys: result
-        });
-
+        res.redirect('/list_Sellers');
       });
-    },
-    getHome: (req, res) => {
+    }
+  });
+},
+deleteSeller: (req, res) => {
+  let SellerId = req.params.SellerId;
 
-        res.render('index.ejs', {
-            title: "Database Designers Pro!"
-        });
+  let deleteUserQuery = 'DELETE FROM Seller WHERE SellerId = '+ SellerId;
 
-    },
 
-    getItem: (req, res) => {
 
-    	let ItemQ = "SELECT * FROM Item ORDER BY ItemID ASC";
-		db.query(ItemQ, (err, result) => {
-			if (err) res.redirect('/');
+  db.query(deleteUserQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.redirect('/list_Sellers');
+  });
 
-			res.render('list_items.ejs', {title: "List Items",
-				Item: result
-			});
 
-		});
-    },
+},
 
-    getSellerPage:  (req, res) => {
-        let ItemQ = "SELECT * FROM Seller ORDER BY SellerId ASC";
-        db.query(ItemQ, (err, result) => {
+deleteItem: (req, res) => {
+  let ItemId = req.params.ItemId;
+
+  let deleteUserQuery = 'DELETE FROM Item WHERE ItemId = '+ ItemId;
+
+
+
+  db.query(deleteUserQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.redirect('/list_Items');
+  });
+
+
+},
+
+addItemPage: (req, res) =>{
+  res.render('add_item.ejs', {
+    title: "Add an Item!!!",
+    message: ''
+  });
+},
+
+addItem: (req, res) => {
+
+  if(req.body.ItemName == '' || req.body.Price == '' || req.body.ItemType == '' || req.body.Quantity == '' || req.body.SellerId == ''){
+    res.direct('list_items.ejs');
+  }
+
+  let message1 ='';
+  let ItemName = req.body.ItemName;
+  let Price = req.body.Price;
+  let ItemType = req.body.ItemType;
+  let Quantity = req.body.Quantity;
+  let SellerId = req.body.SellerId;
+
+
+  let query = "INSERT INTO Item (Price, ItemType,Quantity,ItemName, SellerId) VALUES (" + Price + ",'" + ItemType + "'," + Quantity + ",'" + ItemName + "'," + SellerId + ")";
+
+  db.query(query, (err,result) => {
+    if(err) {
+      return res.status(500).send(err);
+    }
+    res.redirect('/list_Items');
+  });
+
+
+},
+
+addBuy: (req, res) => {
+  if(req.body.buyQuantity == '')
+  {
+    res.direct('list_items.ejs');
+  }
+  console.log(req.body);
+  let quant = req.body.buyQuant;
+  let id = req.body.ItemId;
+  let price = req.body.ItemPrice;
+  let curQuant = req.body.curQuant;
+  console.log(quant);
+  if(quant > curQuant)
+  {
+    message1 = 'Desried quantity  exceeds the current stock.';
+    res.render('list_Items.ejs',{
+      message: message1,
+      title: "Value too high."
+    });
+  }
+  else {
+    console.log(user);
+    let query = "insert into Buys (CustomerId, ItemId, Quantity, Price, PaymentId) values ('"+user+"', " + id + ", " + quant + ", " + (price * quant) + ", NULL);" ;
+    let q2 = "update Item I set I.Quantity = " + (curQuant - quant) + " where I.ItemId = " + id;
+    db.query(query, (err, result) => {
+      console.log(result);
+      if(err){
+        return res.status(500).send(err);
+      }
+
+
+      db.query(q2, (err, result2)=>{let ItemQ = "SELECT * FROM Item ORDER BY ItemID ASC";
+        db.query(ItemQ, (err, r) => {
+
+          if(err){ throw err;}
+
+          else{
             if (err) res.redirect('/');
 
-            res.render('list_sellers.ejs', {title: "List Sellers!",
-                Seller: result
+            res.render('list_items.ejs', {title: "List Items",
+              Item: r
             });
-
-        });
-    },
-
-
-    getSeller: (req, res) => {
-    	let ItemQ = "SELECT * FROM Seller ORDER BY SellerId ASC";
-		db.query(ItemQ, (err, result) => {
-			if (err) res.redirect('/');
-
-			res.render('list_sellers.ejs', {title: "List Sellers!",
-				Seller: result
-			});
-
-		});
-    },
-
-    addSellerPage: (req, res) =>{
-        res.render('add_seller.ejs', {
-            title: "Add a Seller!",
-            message: ''
-        });
-    },
-
-    addSeller: (req, res) => {
-
-        if(req.body.SellerName == ''){
-            res.direct('list_sellers.ejs');
-        }
-
-        let message1 ='';
-        let SellerName = req.body.SellerName;
-
-        let nameQuery= "Select * from Seller where SellerName = '" + SellerName + "'";
-        db.query(nameQuery, (err, result)=>{
-            if(err){
-                return res.status(500).send(err);
-            }
-
-            if(result.length >0 && SellerName != null){
-                message1 = 'Name already exists in the database or is null';
-                res.render('add_seller.ejs',{
-                    message: message1,
-                    title: "Add a Seller!"
-                });
-
-            } else {
-
-                let query = "INSERT INTO Seller (SellerName) VALUES ('" + SellerName + "')";
-
-                db.query(query, (err,result) => {
-                    if(err) {
-                        return res.status(500).send(err);
-                    }
-                    res.redirect('/list_Sellers');
-                });
-            }
-        });
-    },
-    deleteSeller: (req, res) => {
-        let SellerId = req.params.SellerId;
-
-        let deleteUserQuery = 'DELETE FROM Seller WHERE SellerId = '+ SellerId;
-
-
-
-                db.query(deleteUserQuery, (err, result) => {
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                    res.redirect('/list_Sellers');
-                });
-
-
-    },
-
-     deleteItem: (req, res) => {
-        let ItemId = req.params.ItemId;
-
-        let deleteUserQuery = 'DELETE FROM Item WHERE ItemId = '+ ItemId;
-
-
-
-                db.query(deleteUserQuery, (err, result) => {
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                    res.redirect('/list_Items');
-                });
-
-
-    },
-
-    addItemPage: (req, res) =>{
-        res.render('add_item.ejs', {
-            title: "Add an Item!!!",
-            message: ''
-        });
-    },
-
-    addItem: (req, res) => {
-
-        if(req.body.ItemName == '' || req.body.Price == '' || req.body.ItemType == '' || req.body.Quantity == '' || req.body.SellerId == ''){
-            res.direct('list_items.ejs');
-        }
-
-        let message1 ='';
-        let ItemName = req.body.ItemName;
-        let Price = req.body.Price;
-        let ItemType = req.body.ItemType;
-        let Quantity = req.body.Quantity;
-        let SellerId = req.body.SellerId;
-
-
-         let query = "INSERT INTO Item (Price, ItemType,Quantity,ItemName, SellerId) VALUES (" + Price + ",'" + ItemType + "'," + Quantity + ",'" + ItemName + "'," + SellerId + ")";
-
-                db.query(query, (err,result) => {
-                    if(err) {
-                        return res.status(500).send(err);
-                    }
-                    res.redirect('/list_Items');
-                });
-
-
-    },
-
-    addBuy: (req, res) => {
-      if(req.body.buyQuantity == '')
-      {
-        res.direct('list_items.ejs');
-      }
-      console.log(req.body);
-      let quant = req.body.buyQuant;
-      let id = req.body.ItemId;
-      let price = req.body.ItemPrice;
-      let curQuant = req.body.curQuant;
-      console.log(quant);
-      if(quant > curQuant)
-      {
-        message1 = 'Desried quantity  exceeds the current stock.';
-        res.render('list_Items.ejs',{
-            message: message1,
-            title: "Value too high."
-          });
-        }
-      else {
-        let query = "insert into Buys (CustomerId, ItemId, Quantity, Price, PaymentId) values (1, " + id + ", " + quant + ", " + (price * quant) + ", NULL);" ;
-          let q2 = "update Item I set I.Quantity = " + (curQuant - quant) + " where I.ItemId = " + id;
-        db.query(query, (err, result) => {
-          console.log(result);
-          if(err){
-            return res.status(500).send(err);
           }
-
-          let ItemQ = "SELECT * FROM Item ORDER BY ItemID ASC";
-        db.query(ItemQ, (err, r) => {
-          if (err) res.redirect('/');
-
-          res.render('list_items.ejs', {title: "List Items",
-            Item: r
-          });
-          });
         });
-      }
+      });
 
-    },
+      
+    });
+  }
 
-    userLogin: (req,res) => {
-        if(req.body.loginUser == null || req.body.loginPass == null || req.body.loginUser == '' || req.body.loginPass == '')
+},
 
-        {
-          console.log("err at line 219");
-            res.redirect("/");
-        }else{
+userLogin: (req,res) => {
+  if(req.body.loginUser == null || req.body.loginPass == null || req.body.loginUser == '' || req.body.loginPass == '')
 
-        
-        let q = "select CustomerId from Customer where CustomerId = '" + req.body.loginUser + "' and Pass = 'SHA2(" + req.body.loginPass + ", 256)' ;";
-        db.query(q, (err, result) => {
-            if(err)
-            {
-              console.log(err);
-             console.log("Error in UserLogin");
-             console.log(result);
-             console.log(req.body);
-             res.redirect("/");
-            }else{
-            console.log(result);
-            res.redirect("/");
-          }
+  {
+    console.log("err at line 219");
+    res.redirect("/");
+  }else{
 
-        });
-      }
-    },
-    userReg: (req,res) => {
-      console.log(req.body)
-        if(req.body.regUser == '' || req.body.regPass == '')
-        {
-          console.log(req.body);
-          res.redirect("/");
-        }else{
-
-        
-        let q = "insert into Customer(CustomerId, Pass, FirstName, LastName, Address) values ('" + req.body.regUser + "', 'SHA2(" + req.body.regPass + ", 256)', NULL, NULL, NULL);";
+    
+    let q = "select CustomerId from Customer where CustomerId = '" + req.body.loginUser + "' and Pass = 'SHA2(" + req.body.loginPass + ", 256)' ;";
+    db.query(q, (err, result) => {
+      if(err)
+      {
+        console.log(err);
+        console.log("Error in UserLogin");
+        console.log(result);
         console.log(req.body);
-        db.query(q, (err, result) => {
-          if(err)
-          {
-            cconsole.log("Error in UserReg");
-            res.redirect("/");
-          }else{
-
-
-          console.log(result);
-          console.log(req.body);
-          res.redirect("/");
-        }
-
-        });
+        res.redirect("/");
+        
+      }else{
+       if(user == 'NULL'){
+        user = req.body.loginUser
+      }else{
+        user = req.body.loginUser
       }
-    },
-
-    signup: (req, res) =>{
-       res.render('signup.ejs', {
-            title: "Database Designers Pro Signup!"
-        });
-
-
-    },
-
-    login: (req, res) =>{
-       res.render('login.ejs', {
-            title: "Database Designers Pro login!"
-        });
-
-
+      console.log(result);
+      res.redirect("/");
     }
+
+  });
+  }
+},
+userReg: (req,res) => {
+  console.log(req.body)
+  if(req.body.regUser == '' || req.body.regPass == '')
+  {
+    console.log(req.body);
+    res.redirect("/");
+  }else{
+
+    
+    let q = "insert into Customer(CustomerId, Pass, FirstName, LastName, Address) values ('" + req.body.regUser + "', 'SHA2(" + req.body.regPass + ", 256)', NULL, NULL, NULL);";
+    console.log(req.body);
+    db.query(q, (err, result) => {
+      if(err)
+      {
+        console.log("Error in UserReg");
+        res.redirect("/");
+      }else{
+        if(user == 'NULL'){
+        user = req.body.regUser
+      }else{
+        user = req.body.regUser
+      }
+
+
+        console.log(result);
+        console.log(req.body);
+        res.redirect("/");
+      }
+
+    });
+  }
+},
+
+signup: (req, res) =>{
+ res.render('signup.ejs', {
+  title: "Database Designers Pro Signup!"
+});
+
+
+},
+
+login: (req, res) =>{
+ res.render('login.ejs', {
+  title: "Database Designers Pro login!"
+});
+
+
+}
 };
 
 
